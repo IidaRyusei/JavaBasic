93a96717065f81d172ccb6f657b1d0002bec1b25
流星 <ryusei.iida.likeit@gmail.com>
2018-10-02 14:22:20 +0900

    1-6

--- Practice/src/practice01/PTra01_06.java
+++ Practice/src/practice01/PTra01_06.java
@@ -9,13 +9,13 @@ package practice01;
 public class PTra01_06 {
 	public static void main(String[] args) {
 
-		// ★ 文字列を格納する変数 str を宣言してください
+		String name;// ★ 文字列を格納する変数 str を宣言してください
 
 
-		// ★ 変数 str に自分の名前を代入してください
+		name="飯田流星";// ★ 変数 str に自分の名前を代入してください
 
 
-		// ★ 変数 str の中身を出力してください
+		System.out.println(name);// ★ 変数 str の中身を出力してください
 
 
 	}
